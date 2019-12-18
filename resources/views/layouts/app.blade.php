31a2c5dca988ba1d5a54d8fb9b60cfe9653dcb56
surasi <pang-_-.pang@hotmail.com>
2019-12-18 23:54:07 +0700

    [designer profile] [80%]
    [html][css][js-progressbar]

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -293,5 +293,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="{{asset('js/activeonclick.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <script src="{{asset('js/progess-step.js')}}"></script>
 </body>
 </html>
