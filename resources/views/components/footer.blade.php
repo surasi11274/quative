9ff8348c7728d16586ea49207f88c784589bcd02
surasi <pang-_-.pang@hotmail.com>
2020-01-05 19:01:47 +0700

    [edit][css][font][home][image]
    @thebiw2004
    pic
    ____
    logo
    product

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+@section('content')
+    <footer class="page-footer font-small blue pt-4" style="background-color: #904ae8
+;">
+
+        <!-- Footer Links -->
+        <div class="container-fluid text-center text-md-left " >
+
+            <!-- Grid row -->
+            <div class="row justify-content-center">
+
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none pb-3">
+
+                <!-- Grid column -->
+                <div class="text-center">
+
+                    <!-- Links -->
+                    <!-- <h5 class="text-uppercase"></h5> -->
+
+                    <ul class="list-unstyled mx-auto">
+                        <li>
+                            <a><img src="https://sv1.picz.in.th/images/2019/12/17/isMg0I.png" width="50" height="50" alt=""></a>
+                        </li>
+                        <li>
+                            <a href="#" style="font-family: prompt; font-weight: 400;  color: white; font-size: 24px;">Quative</a>
+                        </li>
+                        <div class="row" >
+                            <li>
+                                <a href="#!" style="color: white;">หน้าแรก</a>
+                            </li>
+                            <li>
+                                <a href="#!" style="margin-left: 10px; color: white;">ค้นหานักออกแบบ</a>
+                            </li>
+
+                            <li>
+                                <a href="#!" style="margin-left: 10px; color: white;">ผลงานนักออกแบบ</a>
+                            </li>
+                        </div>
+
+
+                        <!-- <div class="row" >
+                          <li>
+                                  <a href="#!" style="color: white;">เข้าสู่ระบบ |</a>
+                          </li>
+                          <li>
+                                  <a href="#!" style="margin-left: 5px; color: white;">ค้นหานักออกแบบ</a>
+                          </li>
+
+                          <li>
+                                  <a href="#!" style="margin-left: 5px; color: white;">ผลงานนักออกแบบ</a>
+                          </li>
+                        </div> -->
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Footer Links -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3" style="background-color: white; color:#904ae8
+;">    <a href="#" style="color:#904ae8
+;"> quative</a>
+            © copyright 2019. All rights reserved.
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    @endsection
