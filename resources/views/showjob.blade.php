62e1b904e8b29d95e74a130ab8d47eaa3bac157e
BEYOU <thebiw20041@gmail.com>
2020-01-12 01:49:47 +0700

    black not tit

--- resources/views/showjob.blade.php
+++ resources/views/showjob.blade.php
@@ -4,7 +4,7 @@
 @endsection
 @section('content')
 <div class="container">
-    <div class="black-bg mt_ex p-5">
+    <div class="_black-bg mt_ex p-5">
         <div class="row">
             <div class="col-12 col-md-6 ">
                     <p  class="content-bg mb-5" >ข้อมูลการจ้างงาน <span>no. W0001</span></p>
