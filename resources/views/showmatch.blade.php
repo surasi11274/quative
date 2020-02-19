272566785848a5b4d9f6f4c5b80fd6c1ff815314
surasi <pang-_-.pang@hotmail.com>
2020-02-19 16:50:59 +0700

    [css][base][style_match][headerLink][blade][showmatch]

--- resources/views/showmatch.blade.php
+++ resources/views/showmatch.blade.php
@@ -39,7 +39,7 @@
 
                   <input  type="hidden" class="detaill-select " name="designer_id" plachholder="sadas" id="output">
 
-                  <div class="col-12 col-md-4 shadow-sm">
+                  <div class="col-12 col-md-4 ">
                      <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                         @foreach ($designers as $count => $designer)
                            <a class="nav-link shadow-sm m-1"  id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home-{{ $designer->id }}" onclick="addCart('{{$designer->id}}')" role="tab" aria-controls="v-pills-home" aria-selected="true">
@@ -50,13 +50,14 @@
                                  </span>
                                  <span class="col-9">
                                     <p>{{$designer->name}}</p>
+                                    {{-- <p>{{$designer->surname}}</p> --}}
                                  <i class="fas fa-star star1"></i>
                                     <i class="fas fa-star star1"></i>
                                     <i class="fas fa-star star1"></i>
                                     <i class="fas fa-star star1"></i>
                                     <i class="fas fa-star star1"></i>
                                  <small>(4.6)</small>
-                                 <button href="" class="btn _primary-btn" style="height:50px; width:189px; margin:0px auto;">ดูโปรไฟล์</button>
+                                 {{-- <button href="" class="btn _primary-btn" style="height:50px; width:189px; margin:0px auto;">ดูโปรไฟล์</button> --}}
                            
                                  </span>
                               </span>
@@ -75,14 +76,57 @@
                         @foreach ($designers as $count => $designer)
 
                            <div @if($count == 0) class="tab-pane fade show active"  @else class="tab-pane" @endif  id="v-pills-home-{{$designer->id}}" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                              <h2 class="selectfillter ">  ผลงานบรรณจุภันฑ์ (<small>{{ $designer->id}}</small>)</h2>
                               <div class="overflow-gallery grid-gallery">
                               <div class="row">
+                                 <div class="col-auto">
+                                    <img src="/{{$designer->profilepic}}" class="rounded-circle" alt="..." style="width: 50px;height:50px;">
+                                 </div>
+                                    <div class="col-9">
+                                 <h1>{{$designer->name}}</h1>
+                                    <h5 style="color: #C4C4C4;">นักออกแบบ</h5>   
+                                    </div>             
+                                    <div class="col-12">
+                                       <div class="form-tags ">
+                                          <ul class=" d-flex">
+                                       <i class="fas fa-tag icons p-1"></i>
+                                             
+                                             <li>
+                                                <div class="box-tags p-1">
+                                                   <small>
+                                                      มินิมอล
+                                                   </small>
+                                                </div>
+                                             </li>
+                                             <li>
+                                                <div class="box-tags p-1">
+                                                   <small>
+                                                      ทันสมัย
+                                                   </small>
+                                                </div>
+                                             </li>
+                                             <li>
+                                                <div class="box-tags p-1">
+                                                   <small>
+                                                      วินเทจ
+                                                   </small>
+                                                </div>
+                                             </li>
+                                          </ul>
+                                       </div>
+                                   
+                                       
+                                    </div>            
+                                    <div class="col-12">
+                                       <p>{{$designer->description}}</p>
+                                       <h2 class="selectfillter ">  ผลงานบรรณจุภันฑ์ (<small>{{ $designer->id}}</small>)</h2>   
+                                    </div>        
+                                    
                                  @foreach ($refs as $ref)
-                                 <div class="col-7 mt-3">
+                                 <div class="col-12 mt-3">
+                                    
                                        <img class="rounded"  style=" object-fit: cover;"src="{{ $ref->img}}" />
                                  </div>
-                                 <div class="col-5 mt-3">
+                                 {{-- <div class="col-5 mt-3">
                                     <img class="rounded"  style=" object-fit: cover;"src="{{ $ref->img}}" />
                               </div>
                               <div class="col-5 mt-3">
@@ -90,7 +134,7 @@
                            </div>
                            <div class="col-7 mt-3">
                               <img class="rounded"  style=" object-fit: cover;"src="{{ $ref->img}}" />
-                        </div>
+                        </div> --}}
                                  @endforeach
                                  {{-- <div class="col-5 mt-3">
                                        <img class="rounded" style=" object-fit: cover;"src="photo/@product-7.png" />
