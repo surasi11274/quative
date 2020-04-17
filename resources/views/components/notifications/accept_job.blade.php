ae64fe048f5ec50d729ca969601e225d3fe4692e
surasi <pang-_-.pang@hotmail.com>
2020-04-18 05:08:23 +0700

    over-wrap

--- resources/views/components/notifications/accept_job.blade.php
+++ resources/views/components/notifications/accept_job.blade.php
@@ -34,9 +34,9 @@
 </div>
 <div class="col-9">
     <a href="{{route('job.show', $notification->data['jobs']['token'])}}" style="text-decoration:none;"">
-        <label class="font-weight-bold" for="name">{{$notification->data['user']['name']}} </label><br>
+        <label class="font-weight-bold over-wrap" for="name">{{$notification->data['user']['name']}} </label><br>
     
-        <small class="ml-2 " style="color:#523EE8;">Accept your job W{{$notification->data['jobs']['id']}}
+        <small class="ml-2 over-wrap" style="color:#523EE8;">Accept your job W{{$notification->data['jobs']['id']}}
         </small>
         <br>
             <small>{{$notification->created_at->diffForHumans()}}</small>
