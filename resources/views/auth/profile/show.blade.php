0780a9ee4fd9358da192575d2bc52a108b6e0fde
surasi <pang-_-.pang@hotmail.com>
2020-04-22 18:00:38 +0700

    margin update show de/user

--- resources/views/auth/profile/show.blade.php
+++ resources/views/auth/profile/show.blade.php
@@ -57,7 +57,7 @@
 
             </div>
 
-            <div class="col-12 col-md-6 mt-2">
+            <div class="col-12 col-md-6">
             <h5 class="font-weight-bold">ยืนยันตัวตน</h5>
             <div class="row">
               <div class="col-2">
--- resources/views/designer/show.blade.php
+++ resources/views/designer/show.blade.php
@@ -78,7 +78,7 @@
                 <small class="d-md-none">{{ $jobs->count() }} ครั้ง</small>
             </div>
 
-            <div class="col-12 col-md-6 mt-2">
+            <div class="col-12 col-md-6 ">
             <h5 class="font-weight-bold">ยืนยันตัวตน</h5>
             <div class="row">
                 <div class="col-2">
